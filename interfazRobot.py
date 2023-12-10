8cf1109d26fffd0105bad1e5b73a12979735d5d8
Hayyim Del Rio Reynoso <hayyim@MacBook-Air-de-Hayyim.local>
2023-12-09 19:21:40 -0600

    second commit

--- interfazRobot.py
+++ interfazRobot.py
@@ -1,9 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QLineEdit
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QGridLayout, QApplication
 import serial
 from PyQt5.QtCore import QTimer
 
-arduino_port = '/dev/cu.usbmodem1101'
+arduino_port = '/dev/cu.usbserial-110'
 baud_rate = 9600
 
 # Variable global para almacenar el sentido
@@ -18,11 +18,15 @@ class MainWindow(QWidget):
     def initUI(self):
         layout = QVBoxLayout()
 
+        grid_layout = QGridLayout()
+
         self.label_velocidad = QLabel('Velocidad:')
         self.textbox_velocidad = QLineEdit(self)
+        self.textbox_velocidad.textChanged.connect(lambda: self.validar_numeros(self.textbox_velocidad))
 
         self.label_pulsos = QLabel('Pulsos:')
         self.textbox_pulsos = QLineEdit(self)
+        self.textbox_pulsos.textChanged.connect(lambda: self.validar_numeros(self.textbox_pulsos))
 
         self.button_izquierdo = QPushButton('Izquierdo', self)
         self.button_izquierdo.clicked.connect(lambda: self.imprimir_y_enviar('Izquierdo'))
@@ -33,60 +37,74 @@ class MainWindow(QWidget):
         self.button_enviar = QPushButton('Enviar', self)
         self.button_enviar.clicked.connect(self.enviar_dato)
 
-        layout.addWidget(self.label_velocidad)
-        layout.addWidget(self.textbox_velocidad)
+        # Agregar widgets al diseño de la cuadrícula
+        grid_layout.addWidget(self.label_velocidad, 0, 0)
+        grid_layout.addWidget(self.textbox_velocidad, 0, 1)
 
-        layout.addWidget(self.label_pulsos)
-        layout.addWidget(self.textbox_pulsos)
+        grid_layout.addWidget(self.label_pulsos, 0, 2)
+        grid_layout.addWidget(self.textbox_pulsos, 0, 3)
 
-        layout.addWidget(self.button_izquierdo)
-        layout.addWidget(self.button_derecho)
+        grid_layout.addWidget(self.button_izquierdo, 0, 4)
+        grid_layout.addWidget(self.button_derecho, 0, 5)
 
-        layout.addWidget(self.button_enviar)
+        grid_layout.addWidget(self.button_enviar, 0, 6)
+
+        # Agregar el diseño de la cuadrícula al diseño principal
+        layout.addLayout(grid_layout)
 
         self.setLayout(layout)
 
-        self.setWindowTitle('Interfaz Arduino')
-        self.setGeometry(300, 300, 400, 200)
+        self.setWindowTitle('Control de Robot')
+        self.setGeometry(300, 300, 800, 100)
 
         # Configuración de la comunicación serial
         self.ser = serial.Serial(arduino_port, baud_rate, timeout=1)
 
+        # Configurar un temporizador para verificar los datos recibidos cada 100 ms
+        timer = QTimer(self)
+        timer.timeout.connect(self.mostrar_dato_recibido)
+        timer.start(100)
+
     def imprimir_y_enviar(self, direccion):
         global sentido  # Acceder a la variable global
-        sentido = direccion
+        
+
+        if direccion == 'Izquierdo':
+            sentido = 0
+        else:
+            sentido = 1
+
         print(f'Clic en botón {direccion}')
-        # Aquí puedes agregar lógica adicional si es necesario
-        # Por ahora, simplemente enviamos los datos al Arduino
-        #self.enviar_dato()
+        print(f'Sentido: {sentido}')
 
     def enviar_dato(self):
         velocidad = self.textbox_velocidad.text()
         pulsos = self.textbox_pulsos.text()
 
-        # Usa un separador reconocible, como ","
-        dato = f'{velocidad},{pulsos},{sentido}\n'
-        print(f'Enviando datos: {dato}')
-
-
-
-
-
-        self.ser.write(dato.encode())
+        # Verificar si los cuadros de texto no están vacíos
+        if velocidad and pulsos:
+            # Usa un separador reconocible, como ","
+            dato = f'{velocidad},{pulsos},{sentido}\n'
+            print(f'Enviando datos: {dato}')
+            self.ser.write(dato.encode())
+        else:
+            print("Los campos de velocidad y pulsos no pueden estar vacíos.")
 
     def mostrar_dato_recibido(self):
         if self.ser.in_waiting > 0:
             received_data = self.ser.readline().decode().strip()
             print(received_data)
 
+    def validar_numeros(self, textbox):
+        # Filtrar caracteres no numéricos
+        texto = textbox.text()
+        nuevo_texto = ''.join(c for c in texto if c.isdigit())
+        if texto != nuevo_texto:
+            textbox.setText(nuevo_texto)
+            textbox.setCursorPosition(len(nuevo_texto))
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = MainWindow()
     mainWindow.show()
-
-    # Configurar un temporizador para verificar los datos recibidos cada 100 ms
-    timer = QTimer(mainWindow)
-    timer.timeout.connect(mainWindow.mostrar_dato_recibido)
-    timer.start(100)
-
     sys.exit(app.exec_())
